a2ceb0a65fc5b37c8174a95a929edbdbf658cda3
David Bucciarelli <dade916@gmail.com>
2014-02-02 12:13:09 +0100

    Tuned the navigation menu

---
+++ src/luxrendersession.cpp
@@ -0,0 +1,159 @@
+/***************************************************************************
+ *   Copyright (C) 1998-2013 by authors (see AUTHORS.txt)                  *
+ *                                                                         *
+ *   This file is part of LuxMark.                                         *
+ *                                                                         *
+ *   LuxMark is free software; you can redistribute it and/or modify       *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   LuxMark is distributed in the hope that it will be useful,            *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ *                                                                         *
+ *   LuxMark website: http://www.luxrender.net                             *
+ ***************************************************************************/
+
+#include <boost/thread.hpp>
+#include <boost/filesystem.hpp>
+
+#include "luxrendersession.h"
+#include "mainwindow.h"
+
+using namespace luxrays;
+using namespace luxcore;
+
+LuxRenderSession::LuxRenderSession(const std::string &fileName, const LuxMarkAppMode mode,
+		const string &devSel) {
+    renderMode = mode;
+    
+    sceneFileName = fileName;
+    deviceSelection = devSel;
+
+	config = NULL;
+	session = NULL;
+
+	started = false;
+}
+
+LuxRenderSession::~LuxRenderSession() {
+	if (started)
+		Stop();
+}
+
+void LuxRenderSession::Start() {
+	assert (!started);
+	started = true;
+
+	// Load the configuration from file
+	Properties props(sceneFileName.c_str());
+	props << Property("screen.refresh.interval")(2000);
+
+	switch (renderMode) {
+		//----------------------------------------------------------------------
+		// BENCHMARK and STRESSTEST
+		//----------------------------------------------------------------------
+		case STRESSTEST_OCL_GPU:
+		case BENCHMARK_OCL_GPU: {
+			props <<
+					Property("opencl.gpu.use")(true) <<
+					Property("opencl.cpu.use")(false) <<
+					Property("renderengine.type")("PATHOCL");
+			break;
+		}
+		case STRESSTEST_OCL_CPUGPU:
+		case BENCHMARK_OCL_CPUGPU: {
+			props <<
+					Property("opencl.gpu.use")(true) <<
+					Property("opencl.cpu.use")(true) <<
+					Property("renderengine.type")("PATHOCL");
+			break;
+		}
+		case STRESSTEST_OCL_CPU:
+		case BENCHMARK_OCL_CPU: {
+			props <<
+					Property("opencl.gpu.use")(false) <<
+					Property("opencl.cpu.use")(true) <<
+					Property("renderengine.type")("PATHOCL");
+			break;
+		}
+		case BENCHMARK_OCL_CUSTOM: {
+			// At the first run, hardwareTreeModel is NULL
+			if (deviceSelection == "") {
+				props <<
+						Property("opencl.gpu.use")(true) <<
+						Property("opencl.cpu.use")(false) <<
+						Property("renderengine.type")("PATHOCL");
+			} else {
+				props <<
+						Property("opencl.devices.select")(deviceSelection) <<
+						Property("renderengine.type")("PATHOCL");
+			}
+			break;
+		}
+		case BENCHMARK_HYBRID_GPU: {
+			props <<
+					Property("opencl.gpu.use")(true) <<
+					Property("opencl.cpu.use")(false) <<
+					Property("renderengine.type")("PATHHYBRID");
+			break;
+		}
+		case BENCHMARK_HYBRID_CUSTOM: {
+			// At the first run, hardwareTreeModel is NULL
+			if (deviceSelection == "") {
+				props <<
+						Property("opencl.gpu.use")(true) <<
+						Property("opencl.cpu.use")(false) <<
+						Property("renderengine.type")("PATHHYBRID");
+			} else {
+				props <<
+						Property("opencl.devices.select")(deviceSelection) <<
+						Property("renderengine.type")("PATHHYBRID");
+			}
+			break;
+		}
+		case BENCHMARK_NATIVE: {
+			props <<
+					Property("renderengine.type")("PATHCPU");
+			break;
+		}
+		default: {
+			LM_LOG("<FONT COLOR=\"#ff0000\">Unknown render mode in LuxRenderSession::RenderthreadImpl(): " << renderMode << "</FONT>");
+			break;
+		}
+	}
+
+	config = new RenderConfig(props);
+	session = new RenderSession(config);
+
+	session->Start();
+}
+
+void LuxRenderSession::Stop() {
+	assert (started);
+
+	delete session;
+	delete config;
+}
+
+const float *LuxRenderSession::GetFrameBuffer() const {
+	return session->GetFilm().GetChannel<float>(Film::CHANNEL_RGB_TONEMAPPED);
+}
+
+u_int LuxRenderSession::GetFrameBufferWidth() const {
+	return session->GetFilm().GetWidth();
+}
+
+u_int LuxRenderSession::GetFrameBufferHeight() const {
+	return session->GetFilm().GetHeight();
+}
+
+const Properties &LuxRenderSession::GetStats() const {
+	session->UpdateStats();
+	return session->GetStats();
+}
---
+++ src/luxvrdialog.cpp
@@ -0,0 +1,129 @@
+ /***************************************************************************
+ *   Copyright (C) 1998-2013 by authors (see AUTHORS.txt)                  *
+ *                                                                         *
+ *   This file is part of LuxRays.                                         *
+ *                                                                         *
+ *   LuxRays is free software; you can redistribute it and/or modify       *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   LuxRays is distributed in the hope that it will be useful,            *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ *                                                                         *
+ *   LuxRays website: http://www.luxrender.net                             *
+ ***************************************************************************/
+
+#include <boost/lexical_cast.hpp>
+#include <QtGui/qevent.h>
+
+#include "luxmarkcfg.h"
+#include "luxvrdialog.h"
+#include "mainwindow.h"
+
+LuxVRDialog::LuxVRDialog(const char *name,
+		const boost::filesystem::path &path, QWidget *parent) :
+		QDialog(parent), ui(new Ui::LuxVRDialog), sceneName(name), exePath(path),
+		toCloseDialog(false) {
+	ui->setupUi(this);
+
+	QObject::connect(this, SIGNAL(signalLuxVRThreadDone()),
+			this, SLOT(luxVRThreadDone()));
+
+	luxvrThread = new boost::thread(boost::bind(LuxVRDialog::LuxVRThreadImpl, this));
+}
+
+LuxVRDialog::~LuxVRDialog() {
+	luxvrThread->join();
+	delete luxvrThread;
+
+	delete ui;
+}
+
+void LuxVRDialog::closeEvent(QCloseEvent *event) {
+	if (toCloseDialog)
+		event->accept(); // Close window
+	else
+		event->ignore(); // Keep window
+}
+
+void LuxVRDialog::luxVRThreadDone() {
+	toCloseDialog = true;
+
+	// Close the dialog
+	close();
+}
+
+#if defined(WIN32)
+#define POPEN _popen
+#define PCLOSE _pclose
+#else
+#define POPEN popen
+#define PCLOSE pclose
+#endif
+
+void LuxVRDialog::ExecCmd(const string &cmd) {
+	string execCmd = cmd;
+#if defined(WIN32)
+	// Wrap whole command line in quotes so it becomes one parameter
+	execCmd = "\"" + execCmd + "\"";
+#endif
+	LM_LOG("Exec: [" << execCmd << "]");
+
+	FILE *pipe = POPEN(execCmd.c_str(), "r");
+	if (!pipe)
+		throw runtime_error("Unable to execute command: " + cmd);
+
+	LM_LOG("Command output:");
+
+	string line = "";
+	for (;;) {
+		int c = fgetc(pipe);
+		if (c == EOF) {
+			LM_LOG(line);
+			break;
+		} else if (c == '\r') {
+			// Nothing to do
+		} else if (c == '\n') {
+			LM_LOG(line);
+			line = "";
+		} else
+			line += boost::lexical_cast<unsigned char>((unsigned char)c);
+		
+	}
+	PCLOSE(pipe);
+}
+
+void LuxVRDialog::LuxVRThreadImpl(LuxVRDialog *luxvrDialog) {
+	// Looks for LuxVR command
+	LM_LOG("Lux executable path: [" << luxvrDialog->exePath << "]");
+	boost::filesystem::path luxvrPath = luxvrDialog->exePath / "slg4";
+	if (!boost::filesystem::exists(luxvrPath))
+		luxvrPath = luxvrDialog->exePath / "slg4.exe";
+	if (!boost::filesystem::exists(luxvrPath))
+		luxvrPath = luxvrDialog->exePath / "SmallluxGPU/slg4"; // inside OSX bundle
+	if (!boost::filesystem::exists(luxvrPath))
+		throw runtime_error("Unable to find SLG4 executable");
+	LM_LOG("LuxVR path: [" << luxvrPath << "]");
+	const string luxvr = luxvrPath.make_preferred().string();
+	LM_LOG("LuxVR native path: [" << luxvr << "]");
+
+	const string luxvrCmd = "\"" + luxvr + "\" "
+        "-R "
+        "-D renderengine.type RTPATHOCL "
+        "-D screen.refresh.interval 50 "
+        //"-D opencl.devices.select 10000 "
+        " \"" + luxvrDialog->sceneName + "\"" + " 2>&1";
+
+	// Start LuxVR
+	ExecCmd(luxvrCmd);
+
+	LM_LOG("LuxVR done");
+	
+	luxvrDialog->signalLuxVRThreadDone();
+}
