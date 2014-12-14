32bb7ae16ac3629269c8ac0a15db0c9b357e0b0c
Vlad "SATtva" Miller <sattva@vladmiller.info>
2014-12-14 16:10:25 +0600

    Added Embree LD preloading to the wrapper script.

--- src/linux-64bit/LuxMark.sh
+++ src/linux-64bit/LuxMark.sh
@@ -1,6 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
+ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # You may run in trouble on localized Linux installations
 # because of ',' parsed as decimal separator instead of '.' char
+export LC_ALL=C
+# Embree dynamic library is assumed to be located in the 'lib'
+# subdirectory.
+export LD_PRELOAD="$ROOT/lib/libembree.so.2"
 
-LC_ALL=C ./luxmark-linux64 $@
+cd "$ROOT"
+./luxmark.bin "$@"
