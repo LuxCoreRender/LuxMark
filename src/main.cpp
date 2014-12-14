328bba06854809d2b8cbaf54731329687d0ba293
David Bucciarelli <dade916@gmail.com>
2014-12-14 11:33:32 +0100

    Scaled down the size of the HDR map in Mic scene

---
+++ src/main.cpp
@@ -0,0 +1,142 @@
+/***************************************************************************
+ *   Copyright (C) 1998-2013 by authors (see AUTHORS.txt)                  *
+ *                                                                         *
+ *   This file is part of LuxRays.                                         *
+ *                                                                         *
+ *   LuxRays is free software; you can redistribute it and/or modify       *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   LuxRays is distributed in the hope that it will be useful,            *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ *                                                                         *
+ *   LuxRays website: http://www.luxrender.net                             *
+ ***************************************************************************/
+
+#include <cstdio>
+#include <cstdlib>
+
+#include "luxmarkdefs.h"
+#include "mainwindow.h"
+#include "luxmarkapp.h"
+
+static void PrintCmdLineHelp(const QString &cmd) {
+	cerr << "Usage: " << cmd.toAscii().data() << " [options]" << endl <<
+			" --help (display this help and exit)" << endl <<
+			" --scene=LUXBALL_HDR|MICROPHONE|HOTEL (select the scene to use)" << endl <<
+			" --mode="
+                "BENCHMARK_OCL_GPU|BENCHMARK_OCL_CPUGPU|BENCHMARK_OCL_CPU|"
+                "BENCHMARK_HYBRID_GPU|BENCHMARK_NATIVE|"
+                "STRESSTEST_OCL_GPU|STRESSTEST_OCL_CPUGPU|STRESSTEST_OCL_CPU|"
+                "DEMO_LUXVR|PAUSE"
+				" (select the mode to use)" << endl <<
+			" --single-run (run the benchmark, print the result to the stdout and exit)" << endl;
+}
+
+int main(int argc, char **argv) {
+	LuxMarkApp app(argc, argv);
+
+	// Get the arguments into a list
+	bool exit = false;
+	bool singleRun = false;
+
+	QStringList argsList = app.arguments();
+	QRegExp argHelp("--help");
+	QRegExp argScene("--scene=(LUXBALL_HDR|MICROPHONE|HOTEL)");
+	QRegExp argMode("--mode=("
+		"BENCHMARK_OCL_GPU|BENCHMARK_OCL_CPUGPU|BENCHMARK_OCL_CPU|"
+		"BENCHMARK_NATIVE|"
+		"STRESSTEST_OCL_GPU|STRESSTEST_OCL_CPUGPU|STRESSTEST_OCL_CPU|"
+		"DEMO_LUXVR|PAUSE"
+		")");
+	QRegExp argSingleRun("--single-run");
+
+	LuxMarkAppMode mode = BENCHMARK_OCL_GPU;
+	// Remember to change the default label in mainwindow.cpp too
+	const char *scnName = SCENE_LUXBALL_HDR;
+    for (int i = 1; i < argsList.size(); ++i) {
+        if (argHelp.indexIn(argsList.at(i)) != -1 ) {   
+			PrintCmdLineHelp(argsList.at(0));
+            exit = true;
+			break;
+		} else if (argScene.indexIn(argsList.at(i)) != -1) {   
+            QString scene = argScene.cap(1);
+			if (scene.compare("LUXBALL_HDR", Qt::CaseInsensitive) == 0)
+				scnName = SCENE_LUXBALL_HDR;
+			else if (scene.compare("MICROPHONE", Qt::CaseInsensitive) == 0)
+				scnName = SCENE_MICROPHONE;
+			else if (scene.compare("HOTEL", Qt::CaseInsensitive) == 0)
+				scnName = SCENE_HOTEL;
+			else {
+				cerr << "Unknown scene name: " << argScene.cap(1).toAscii().data() << endl;
+				PrintCmdLineHelp(argsList.at(0));
+				exit = true;
+				break;
+			}
+		} else if (argMode.indexIn(argsList.at(i)) != -1 ) {   
+            QString scene = argMode.cap(1);
+			if (scene.compare("BENCHMARK_OCL_GPU", Qt::CaseInsensitive) == 0)
+				mode = BENCHMARK_OCL_GPU;
+			else if (scene.compare("BENCHMARK_OCL_CPUGPU", Qt::CaseInsensitive) == 0)
+				mode = BENCHMARK_OCL_CPUGPU;
+			else if (scene.compare("BENCHMARK_OCL_CPU", Qt::CaseInsensitive) == 0)
+				mode = BENCHMARK_OCL_CPU;
+			else if (scene.compare("BENCHMARK_NATIVE", Qt::CaseInsensitive) == 0)
+				mode = BENCHMARK_NATIVE;
+			else if (scene.compare("STRESSTEST_OCL_GPU", Qt::CaseInsensitive) == 0)
+				mode = STRESSTEST_OCL_GPU;
+			else if (scene.compare("STRESSTEST_OCL_CPUGPU", Qt::CaseInsensitive) == 0)
+				mode = STRESSTEST_OCL_CPUGPU;
+			else if (scene.compare("STRESSTEST_OCL_CPU", Qt::CaseInsensitive) == 0)
+				mode = STRESSTEST_OCL_CPU;
+			else if (scene.compare("DEMO_LUXVR", Qt::CaseInsensitive) == 0)
+				mode = DEMO_LUXVR;
+			else if (scene.compare("PAUSE", Qt::CaseInsensitive) == 0)
+				mode = PAUSE;
+			else {
+				cerr << "Unknown mode name: " << argMode.cap(1).toAscii().data() << endl;
+				PrintCmdLineHelp(argsList.at(0));
+				exit = true;
+				break;
+			}
+		} else if (argSingleRun.indexIn(argsList.at(i)) != -1 ) {   
+			singleRun = true;
+        } else {
+            cerr << "Unknown argument: " << argsList.at(i).toAscii().data() << endl;
+			PrintCmdLineHelp(argsList.at(0));
+			exit = true;
+			break;
+        }
+    }
+
+	if (exit)
+		return EXIT_SUCCESS;
+	else {
+		app.Init(mode, scnName, singleRun);
+
+		// If current directory doesn't have the "scenes" directory, move
+		// to where the executable is
+		if (!boost::filesystem::exists("./scenes")) {
+			// Look for the directory where LuxMark executable is installed
+			boost::filesystem::path exePath(boost::filesystem::initial_path<boost::filesystem::path>());
+			exePath = boost::filesystem::system_complete(boost::filesystem::path(argv[0])).parent_path();
+			// Set the current directory to where the binary is
+			boost::filesystem::current_path(exePath);
+		}
+	
+		// Force C locale
+		setlocale(LC_NUMERIC, "C");
+
+		if (app.mainWin != NULL) {
+			app.mainWin->UpdateSceneLabel(scnName);
+			return app.exec();
+		} else
+			return EXIT_FAILURE;
+	}
+}
---
+++ src/mainwindow.h
@@ -0,0 +1,141 @@
+/***************************************************************************
+ *   Copyright (C) 1998-2013 by authors (see AUTHORS.txt)                  *
+ *                                                                         *
+ *   This file is part of LuxRays.                                         *
+ *                                                                         *
+ *   LuxRays is free software; you can redistribute it and/or modify       *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   LuxRays is distributed in the hope that it will be useful,            *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ *                                                                         *
+ *   LuxRays website: http://www.luxrender.net                             *
+ ***************************************************************************/
+
+#ifndef _MAINWINDOW_H
+#define	_MAINWINDOW_H
+
+#ifndef Q_MOC_RUN
+#include <cstddef>
+#include <string>
+
+#include <boost/thread/mutex.hpp>
+
+#include "ui_mainwindow.h"
+#include "hardwaretree.h"
+#include "luxmarkdefs.h"
+
+#include <QGraphicsPixmapItem>
+#endif
+
+class LuxMarkApp;
+
+class LuxFrameBuffer : public QGraphicsPixmapItem {
+public:
+	LuxFrameBuffer(const QPixmap &pixmap);
+
+	void SetLuxApp(LuxMarkApp *la) { luxApp = la; }
+
+private:
+	LuxMarkApp *luxApp;
+};
+
+class MainWindow : public QMainWindow {
+	Q_OBJECT
+
+public:
+	MainWindow(QWidget *parent = NULL, Qt::WindowFlags flags = 0);
+	~MainWindow();
+
+	void ShowLogo();
+	bool IsShowingLogo() const;
+	void ShowFrameBuffer(const float *frameBuffer,
+		const unsigned int width, const unsigned int height);
+	const unsigned char *GetFrameBuffer() const { return frameBuffer; }
+
+	void SetModeCheck(const LuxMarkAppMode mode);
+	void SetSceneCheck(const int index);
+	void UpdateSceneLabel(const char *name);
+	void UpdateScreenLabel(const char *msg, const bool valid);
+	void SetHardwareTreeModel(HardwareTreeModel *treeModel);
+	void SetLuxApp(LuxMarkApp *la) { luxFrameBuffer->SetLuxApp(la); }
+
+	void Pause();
+
+private:
+	bool event(QEvent *event);
+
+	Ui::MainWindow *ui;
+	QGraphicsPixmapItem *luxLogo;
+	LuxFrameBuffer *luxFrameBuffer;
+	unsigned char *frameBuffer;
+	unsigned int fbWidth, fbHeight;
+	QGraphicsSimpleTextItem *authorLabel;
+	QGraphicsSimpleTextItem *authorLabelBack;
+	QGraphicsSimpleTextItem *screenLabel;
+	QGraphicsRectItem *screenLabelBack;
+	QLabel *statusBarLabel;
+
+	QGraphicsScene *renderScene;
+
+private slots:
+	void exitApp();
+	void showAbout();
+
+	void setLuxBallHDRScene();
+	void setMicrophoneScene();
+	void setHotelScene();
+
+	void setMode_BENCHMARK_OCL_GPU();
+	void setMode_BENCHMARK_OCL_CPUGPU();
+	void setMode_BENCHMARK_OCL_CPU();
+	void setMode_BENCHMARK_OCL_CUSTOM();
+	void setMode_BENCHMARK_NATIVE_PATH();
+	void setMode_STRESSTEST_OCL_GPU();
+	void setMode_STRESSTEST_OCL_CPUGPU();
+	void setMode_STRESSTEST_OCL_CPU();
+	void setMode_DEMO_LUXVR();
+	void setMode_PAUSE();
+};
+
+//------------------------------------------------------------------------------
+// Log related definitions
+//------------------------------------------------------------------------------
+
+extern MainWindow *LogWindow;
+
+class LuxLogEvent: public QEvent {
+public:
+	LuxLogEvent(QString mesg);
+ 
+	QString getMessage() { return message; }
+
+private:
+	QString message;
+};
+
+class LuxErrorEvent: public QEvent {
+public:
+	LuxErrorEvent(QString mesg);
+
+	QString getMessage() { return message; }
+
+private:
+	QString message;
+};
+
+#define LM_LOG(a) { if (LogWindow) { std::stringstream _LM_LOG_LOCAL_SS; _LM_LOG_LOCAL_SS << a; qApp->postEvent(LogWindow, new LuxLogEvent(QString(_LM_LOG_LOCAL_SS.str().c_str()))); }}
+#define LM_LOG_LUXRAYS(a) { LM_LOG("<FONT COLOR=\"#002200\"><B>[LuxRays]</B></FONT> " << a); }
+#define LM_LOG_SDL(a) { LM_LOG("<FONT COLOR=\"#005500\"><B>[SDL]</B></FONT> " << a); }
+#define LM_LOG_LUXCORE(a) { LM_LOG("<FONT COLOR=\"#009900\"><B>[LuxCore]</B></FONT> " << a); }
+
+#define LM_ERROR(a) { if (LogWindow) { std::stringstream _LM_ERR_LOCAL_SS; _LM_ERR_LOCAL_SS << a; qApp->postEvent(LogWindow, new LuxErrorEvent(QString(_LM_ERR_LOCAL_SS.str().c_str()))); }}
+
+#endif	/* _MAINWINDOW_H */
