045d8e6bf0d163cb6df9ef247477c38249a7151e
David Bucciarelli <dade916@gmail.com>
2016-02-01 15:48:24 +0100

    Updated Config_Dade.cmake

--- cmake/SpecializedConfig/Config_Dade.cmake
+++ cmake/SpecializedConfig/Config_Dade.cmake
@@ -9,14 +9,13 @@
 
 MESSAGE(STATUS "Using Dade's Linux Configuration settings")
 
-#SET(BOOST_SEARCH_PATH		"/home/david/projects/luxrender-dev/boost_1_47_0")
-#SET(OPENIMAGEIO_ROOT_DIR	"/home/david/projects/luxrender-dev/oiio-RB-1.3/dist/linux64")
-#SET(OPENEXR_ROOT			"/usr/local")
+SET(BOOST_SEARCH_PATH		"/home/david/projects/luxrender-dev/boost_1_56_0-bin")
+SET(OPENIMAGEIO_ROOT_DIR	"/home/david/projects/luxrender-dev/oiio/dist/linux64")
 
 SET(OPENCL_SEARCH_PATH	"$ENV{AMDAPPSDKROOT}")
 SET(OPENCL_INCLUDEPATH	"${OPENCL_SEARCH_PATH}/include")
-#SET(OPENCL_LIBRARYDIR	"${OPENCL_SEARCH_PATH}/lib/x86_64")
-SET(EMBREE_SEARCH_PATH	"/home/david/src/embree-bin-2.3.3_linux")
+
+SET(EMBREE_SEARCH_PATH		"/home/david/src/embree-bin-2.4_linux")
 
 #SET(CMAKE_BUILD_TYPE "Debug")
 SET(CMAKE_BUILD_TYPE "Release")
