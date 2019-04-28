3c4e9264f8f56e00591a3a772eebd993f3c9ab03
David Bucciarelli <dade916@gmail.com>
2019-04-28 12:39:24 +0200

    Updated to use LinuxCompile Qt5

--- cmake/SpecializedConfig/Config_Dade.cmake
+++ cmake/SpecializedConfig/Config_Dade.cmake
@@ -11,7 +11,7 @@ MESSAGE(STATUS "Using Dade's Linux Configuration settings")
 
 SET(CMAKE_INCLUDE_PATH "/home/david/projects/luxcorerender/LinuxCompile/target-64-sse2/include;/home/david/projects/luxcorerender/LinuxCompile/target-64-sse2")
 SET(CMAKE_LIBRARY_PATH "/home/david/projects/luxcorerender/LinuxCompile/target-64-sse2/lib;/home/david/projects/luxcorerender/LinuxCompile/target-64-sse2")
-SET(Qt5_DIR "/home/david/tmp/Qt/5.12.2/gcc_64/lib/cmake/Qt5")
+SET(Qt5_DIR "/home/david/projects/luxcorerender/LinuxCompile/target-64-sse2/lib/cmake/Qt5")
 
 SET(LuxRays_HOME "/home/david/projects/luxcorerender/LuxCore")
 
--- cmake/SpecializedConfig/Config_Linux-Static.cmake
+++ cmake/SpecializedConfig/Config_Linux-Static.cmake
@@ -7,15 +7,11 @@
 
 #cmake -DLUXRAYS_CUSTOM_CONFIG=cmake/SpecializedConfig/Config_Linux-Static.cmake .
 
-MESSAGE(STATUS "Using my own settings")
+MESSAGE(STATUS "Using Linux static own settings")
 
-set(FREEIMAGE_ROOT            "${LuxRays_SOURCE_DIR}/../target/include")
-set(BOOST_SEARCH_PATH         "${LuxRays_SOURCE_DIR}/../target/lib")
+SET(CMAKE_INCLUDE_PATH "${LuxRays_SOURCE_DIR}/../target-64-sse2/include;${LuxRays_SOURCE_DIR}/../target-64-sse2")
+SET(CMAKE_LIBRARY_PATH "${LuxRays_SOURCE_DIR}/../target-64-sse2/lib;${LuxRays_SOURCE_DIR}/../target-64-sse2")
+SET(Qt5_DIR "${LuxRays_SOURCE_DIR}/../target-64-sse2/lib/cmake/Qt5")
 
-# set(OPENCL_SEARCH_PATH        "$ENV{ATISTREAMSDKROOT}")
-set(OPENCL_SEARCH_PATH        "/usr/src/opencl-sdk/include")
-set(OPENCL_LIBRARYDIR         "${OPENCL_SEARCH_PATH}/lib/x86_64")
-
-# set(BUILD_LUXMARK TRUE)
-set(CMAKE_BUILD_TYPE "Release")
-
+#SET(CMAKE_BUILD_TYPE "Debug")
+SET(CMAKE_BUILD_TYPE "Release")
