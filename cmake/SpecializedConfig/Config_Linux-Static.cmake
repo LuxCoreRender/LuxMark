8b1295dd7545a9620fef9e73a010b2f9fb57b756
David Bucciarelli <dade916@gmail.com>
2019-04-28 17:16:32 +0200

    Fixed Config_Linux-Static.cmake

--- cmake/SpecializedConfig/Config_Linux-Static.cmake
+++ cmake/SpecializedConfig/Config_Linux-Static.cmake
@@ -5,13 +5,13 @@
 #
 ###########################################################################
 
-#cmake -DLUXRAYS_CUSTOM_CONFIG=cmake/SpecializedConfig/Config_Linux-Static.cmake .
+#cmake -DLUXMARK_CUSTOM_CONFIG=cmake/SpecializedConfig/Config_Linux-Static.cmake .
 
 MESSAGE(STATUS "Using Linux static own settings")
 
-SET(CMAKE_INCLUDE_PATH "${LuxRays_SOURCE_DIR}/../target-64-sse2/include;${LuxRays_SOURCE_DIR}/../target-64-sse2")
-SET(CMAKE_LIBRARY_PATH "${LuxRays_SOURCE_DIR}/../target-64-sse2/lib;${LuxRays_SOURCE_DIR}/../target-64-sse2")
-SET(Qt5_DIR "${LuxRays_SOURCE_DIR}/../target-64-sse2/lib/cmake/Qt5")
+SET(CMAKE_INCLUDE_PATH "${LuxMark_SOURCE_DIR}/../target-64-sse2/include;${LuxMark_SOURCE_DIR}/../target-64-sse2")
+SET(CMAKE_LIBRARY_PATH "${LuxMark_SOURCE_DIR}/../target-64-sse2/lib;${LuxMark_SOURCE_DIR}/../target-64-sse2")
+SET(Qt5_DIR "${LuxMark_SOURCE_DIR}/../target-64-sse2/lib/cmake/Qt5")
 
 #SET(CMAKE_BUILD_TYPE "Debug")
 SET(CMAKE_BUILD_TYPE "Release")
