d160d39e26ffe2bf8e9905d6c622b1671814d5a1
David Bucciarelli <dade916@gmail.com>
2019-10-07 14:46:08 +0200

    Release v4.0alpha0

--- cmake/SpecializedConfig/Config_Linux-Static.cmake
+++ cmake/SpecializedConfig/Config_Linux-Static.cmake
@@ -11,7 +11,7 @@ MESSAGE(STATUS "Using Linux static own settings")
 
 SET(CMAKE_INCLUDE_PATH "${LuxMark_SOURCE_DIR}/../target-64-sse2/include;${LuxMark_SOURCE_DIR}/../target-64-sse2")
 SET(CMAKE_LIBRARY_PATH "${LuxMark_SOURCE_DIR}/../target-64-sse2/lib;${LuxMark_SOURCE_DIR}/../target-64-sse2")
-SET(Qt5_DIR "${LuxMark_SOURCE_DIR}/../target-64-sse2/lib/cmake/Qt5")
+#SET(Qt5_DIR "${LuxMark_SOURCE_DIR}/../target-64-sse2/lib/cmake/Qt5")
 
 #SET(CMAKE_BUILD_TYPE "Debug")
 SET(CMAKE_BUILD_TYPE "Release")
